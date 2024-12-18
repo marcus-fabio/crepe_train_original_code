490884698fa2cf564995fa008cdddb9715e9f0d6
Marcus Silva <marcus.silva@enablers.com.br>
2024-12-18 12:56:57 -0400

    feat: load crepe model weights and add new layers

--- crempe.py
+++ crempe.py
@@ -1,6 +1,6 @@
-import os
+# import os
 
-from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.models import Model
 from tensorflow.keras.layers import (
     Input,
     Conv2D,
@@ -15,6 +15,29 @@ from tensorflow.keras.layers import (
 
 
 def crempe() -> Model:
+    # Load CREPE model and return the flattened layer
+    crepe_model = load_crepe_model()
+    crepe_input = crepe_model.input
+    x = crepe_model.layers[-2].output
+
+    # New layer to increase feature space from 2048 (CREPE) to 4096
+    y = Dense(4096, activation='relu', name="feature-space")(x)
+
+    # New output to increase bins resolution:
+    # 6 octaves * 12 semitones per octave * 10 bins per semitone = 720 bins
+    # 100 cents per semitone / 10 bins per semitone = 10 cents per bin
+    y = Dense(720, activation='sigmoid', name="crempe-classifier")(y)
+
+    model = Model(inputs=crepe_input, outputs=y)
+
+    # Freeze CREPE model weights
+    for layer in model.layers[:1]:  # First test: freeze only weights of first layer
+        layer.trainable = False
+
+    return model
+
+
+def crepe() -> Model:
     layers = [1, 2, 3, 4, 5, 6]
     filters = [1024, 128, 128, 128, 256, 512]
     filters_sizes = [512, 64, 64, 64, 64, 64]
@@ -31,31 +54,23 @@ def crempe() -> Model:
 
     y = Permute((2, 1, 3), name="transpose")(y)
     y = Flatten(name="flatten")(y)
-
-    # New layer to increase feature space
-    y = Dense(4096, activation='relu', name="classifier")(y)
-
-    # New output to increase bins resolution:
-    # 6 octaves * 12 semitones per octave * 10 bins per semitone = 720 bins
-    # 100 cents per semitone / 10 bins per semitone = 10 cents per bin
-    y = Dense(720, activation='sigmoid', name="classifier")(y)
+    y = Dense(360, activation='sigmoid', name="crepe-classifier")(y)
 
     model = Model(inputs=x, outputs=y)
 
     return model
 
+
 def load_crepe_model() -> Model:
-    package_dir = os.path.dirname(os.path.realpath(__file__))
+    # package_dir = os.path.dirname(os.path.realpath(__file__))
     filename = "model-full.h5"
-    filepath = os.path.join(package_dir, filename)
-    model = load_model(filename)
+    # filepath = os.path.join(package_dir, filename)
+    model = crepe()
+    model.load_weights(filename)
+
     return model
 
-if __name__ == '__main__':
-    import h5py
 
-    # Replace 'model-full.h5' with the path to your .h5 file
-    with h5py.File('model-full.h5', 'r') as f:
-        print(list(f.attrs.keys()))  # List of file-level attributes
-        print(f.attrs.get('keras_version'))  # Keras version (if available)
-        print(f.attrs.get('backend'))
+if __name__ == '__main__':
+    crempe_model = crempe()
+    crempe_model.summary()
