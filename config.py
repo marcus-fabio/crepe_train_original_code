d148bee0d7ccd1ebb226322fbc08e4c797216bfd
Marcus Fabio <fabio.marcus@gmail.com>
2025-01-01 16:32:20 -0400

    code refactor

--- config.py
+++ config.py
@@ -1,17 +1,23 @@
 import os
 import argparse
-from datetime import datetime
 from typing import List
-from keras.callbacks import ModelCheckpoint, TensorBoard, CSVLogger, EarlyStopping, Callback
+from datetime import datetime
+
 from keras import Model, models
+from keras.callbacks import (
+    ModelCheckpoint,
+    EarlyStopping,
+    TensorBoard,
+    CSVLogger,
+    Callback
+)
 
 import models as crepe_models  # noqa
-
 
 parser = argparse.ArgumentParser('CREPE', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('model', nargs='?', default='crepe',
                     help='name of the model')
-parser.add_argument('experiment_name', nargs='?', default=timestamp(),
+parser.add_argument('experiment_name', nargs='?', default=datetime.now().strftime('%Y-%m-%dT%H_%M_%S'),
                     help='a unique identifier string for this run')
 parser.add_argument('--no-augment', dest='augment', action='store_false',
                     help='an option to disable data augmentation')
@@ -42,8 +48,6 @@ options = vars(parser.parse_args())
 log_dir = os.path.join('experiments', options['experiment_name'])
 os.makedirs(log_dir, exist_ok=True)
 
-def timestamp():
-    return datetime.now().isoformat().replace("-", "").replace(":", "").replace("T", "-").split(".")[0][2:]
 
 def log_path(*components):
     return os.path.join(log_dir, *components)
