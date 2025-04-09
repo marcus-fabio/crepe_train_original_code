9b453dd78a53abeace39a92c435358bf93ec09cd
Marcus Fabio <fabio.marcus@gmail.com>
2025-04-09 02:05:17 -0400

    set sampling rate to 8000

--- transforms.py
+++ transforms.py
@@ -7,7 +7,8 @@ random = Random(42)
 
 def shift(audio, amount):
     # return pyrb.pitch_shift(audio, 16000, amount)
-    return pyrb.pitch_shift(audio, 84108, amount)
+    # return pyrb.pitch_shift(audio, 84108, amount)
+    return pyrb.pitch_shift(audio, 8000, amount)
 
 def normalize(audio, pitch):
     audio = audio - np.mean(audio)
