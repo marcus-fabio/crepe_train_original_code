16527270059f0d7ea8bd2cecd70dce2776aafdf0
Marcus Fabio <fabio.marcus@gmail.com>
2025-04-30 01:33:18 -0400

    implement raw_multipitch_accuracy function

--- evaluation.py
+++ evaluation.py
@@ -9,3 +9,24 @@ def accuracies(true_cents, predicted_cents, cent_tolerence=50):
     rpa = raw_pitch_accuracy(voicing, true_cents, voicing, predicted_cents, cent_tolerence)
     rca = raw_chroma_accuracy(voicing, true_cents, voicing, predicted_cents, cent_tolerence)
     return rpa, rca
+
+def raw_multipitch_accuracy(ref_cent, est_cent, cent_tolerance=50):
+    """
+    Computes Raw Pitch Accuracy (RPA) for multi-pitch estimation.
+
+    :param ref_cent: numpy array of reference frequencies numpy arrays
+    :param est_cent: numpy array of estimated frequencies numpy arrays
+    :param cent_tolerance: acceptable tolerance between reference and estimated frequencies
+    :return: rpa as a float
+    """
+    nonzero_freqs = np.logical_and(est_cent != 0.0, ref_cent != 0.0)
+
+    if np.sum(nonzero_freqs) == 0:
+        return 0.0
+
+    freq_diff_cents = np.abs(ref_cent - est_cent)[nonzero_freqs]
+    correct_frequencies = freq_diff_cents < cent_tolerance
+    voicing = ref_cent != 0.0
+    rpa = np.sum(voicing[nonzero_freqs] * correct_frequencies) / np.sum(voicing)
+
+    return rpa
