b3849be7720687d0f11494b868922b4e87b6429f
Marcus Fabio <fabio.marcus@gmail.com>
2025-05-13 17:01:01 -0400

    set load weights crepe option default value as None

--- config.py
+++ config.py
@@ -40,11 +40,10 @@ parser.add_argument('--load-model', default=None,
                     help='when specified, the full model will be loaded from this path')
 parser.add_argument('--load-model-weights', default=None,
                     help='when specified, the model weights will be loaded from this path')
-parser.add_argument('--load-weights-crepe', default='model-full.h5',
-                    help='when specified, the model weights will be loaded from this path')
+parser.add_argument('--load-weights-crepe', default=None,
+                    help='when specified, the crepe model weights will be loaded from this path')
 parser.add_argument('--save-model', default='model.weights.h5',
                     help='path to save the model on each epoch')
-# parser.add_argument('--save-model-weights', default='model.weights.h5',
 parser.add_argument('--save-model-weights', default='weights/model-{epoch:02d}-{val_loss:.2f}.weights.h5',
                     help='path to save the model weights on each epoch; supersedes --save-model')
 parser.add_argument('--save-best-only', default=False, action='store_true',
