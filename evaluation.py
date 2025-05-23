d8da90f2e799aa7df1995bece1db5fa17370c40d
Marcus Silva <marcus.silva@enablers.com.br>
2025-05-23 00:10:16 -0400

    refactor raw pitches accuracy function to evaluate min frequencies distances

--- evaluation.py
+++ evaluation.py
@@ -1,33 +1,54 @@
 import numpy as np
 from mir_eval.melody import raw_pitch_accuracy, raw_chroma_accuracy
+from mir_eval.multipitch import (
+    compute_num_true_positives,
+    # compute_err_score,
+    compute_num_freqs,
+    compute_accuracy
+)
 
 
-def accuracies(true_cents, predicted_cents, cent_tolerence=50):
+def accuracies(true_cents, predicted_cents, cent_tolerance=50):
     assert true_cents.shape == predicted_cents.shape
 
     voicing = np.ones(true_cents.shape)
-    rpa = raw_pitch_accuracy(voicing, true_cents, voicing, predicted_cents, cent_tolerence)
-    rca = raw_chroma_accuracy(voicing, true_cents, voicing, predicted_cents, cent_tolerence)
+    rpa = raw_pitch_accuracy(voicing, true_cents, voicing, predicted_cents, cent_tolerance)
+    rca = raw_chroma_accuracy(voicing, true_cents, voicing, predicted_cents, cent_tolerance)
     return rpa, rca
 
-def raw_multipitch_accuracy(ref_cent, est_cent, cent_tolerance=50):
-    """
-    Computes Raw Pitch Accuracy (RPA) for multi-pitch estimation.
 
-    :param ref_cent: numpy array of reference frequencies arrays => np.array([[2000.03,550.56],[1000.20, 3654.12], ...])
-    :param est_cent: numpy array of estimated frequencies arrays => np.array([[2000.03,550.56],[1000.20, 3654.12], ...])
-    :param cent_tolerance: acceptable tolerance between reference and estimated frequencies
-    :return: rpa as a float
+def raw_pitches_accuracy(ref_cents, est_cents, cent_tolerance=50) -> float:
     """
-    nonzero_freqs = np.logical_and(est_cent != 0.0, ref_cent != 0.0)
+    Computes Raw Pitches Accuracy (RPA) for multi pitches estimation.
+
+    :param ref_cents: reference frequencies in cents => np.array([[2000.03, 550.56,...],[1000.20, 3654.12,...], ...])
+    :param est_cents: estimated frequencies in cents => np.array([[2000.03, 550.56,...],[1000.20, 3654.12,...], ...])
+    :param cent_tolerance: acceptable tolerance between reference and estimated frequencies
+    :return: rpa
+    """
+
+    # remove frames containing only zero frequencies in reference or estimation
+    nonzero_freqs = np.logical_and(np.any(ref_cents, axis=1) != 0.0, np.any(est_cents, axis=1) != 0.0)
 
     if np.sum(nonzero_freqs) == 0:
         return 0.0
 
-    # est_cent array is sorted by model, but ref_cents must be sorted to match est_cents array
-    freq_diff_cents = np.abs(np.sort(ref_cent) - est_cent)[nonzero_freqs]
-    correct_frequencies = freq_diff_cents < cent_tolerance
-    voicing = ref_cent != 0.0
-    rpa = np.sum(voicing[nonzero_freqs] * correct_frequencies) / np.sum(voicing)
+    # keep only non zeros frequencies in frames
+    ref_cent_nz = [cent[cent != 0.0] for cent in ref_cents[nonzero_freqs]]
+    est_cent_nz = [cent[cent != 0.0] for cent in est_cents[nonzero_freqs]]
+
+    # calculate number of true positives, references and estimation for each frame
+    true_positives = compute_num_true_positives(ref_cent_nz, est_cent_nz, cent_tolerance)
+    n_ref = compute_num_freqs(ref_cent_nz)
+    n_est = compute_num_freqs(est_cent_nz)
+
+    # calculate rpa using recall (recall and rpa have same value when keep only non zeros frequencies )
+    _, rpa, _ = compute_accuracy(true_positives, n_ref, n_est)
+    # errors = compute_err_score(true_positives, n_ref, n_est)
+
+    # rpa calculated without mir_eval functions (it works, same result)
+    # total_true_positives = np.sum(true_positives)
+    # total_ref = np.sum(ref_cent_nz != 0.0)
+    # rpa = total_true_positives / total_ref
 
     return rpa
